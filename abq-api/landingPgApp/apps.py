0ee5e37c760d6a30aa4257699c01ea76fc54cfac
Hugo Brito <hugomdmb@gmail.com>
2025-09-10 23:51:49 -0300

    fix: app name

--- abq-api/landingPgApp/apps.py
+++ abq-api/landingPgApp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class LandingapiConfig(AppConfig):
+class LandingPgAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'landingApi'
+    name = 'landingPgApp'
